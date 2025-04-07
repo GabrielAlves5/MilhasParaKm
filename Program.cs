9b078e82b9654b2d90dc1b6db064c0e5ec05f3a2
GabrielAlves5 <ecgabriel.alves@gmail.com>
2025-04-07 14:26:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿double milha;
+Console.WriteLine("Converta Milha para KM");
+Console.WriteLine("Coloque a velocidade em Milhas");
+milha = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"A Milha em KM é: {(milha * 1.609)}KM");
